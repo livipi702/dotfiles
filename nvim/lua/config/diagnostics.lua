2ed0dcfdc0f59141492b40f84f672fc42793cd59
livipi702 <livipi7028@videobix.com>
2026-09-14 17:59:59 +0530

    feat(nvim,nvim-java): yank line diagnostics with <leader>cy, fix gK toggle

--- nvim/lua/config/diagnostics.lua
+++ nvim/lua/config/diagnostics.lua
@@ -11,5 +11,20 @@ vim.diagnostic.config({
 
 vim.keymap.set("n", "gK", function()
   local cur = vim.diagnostic.config().virtual_lines
-  vim.diagnostic.config({ virtual_lines = not cur })
+  vim.diagnostic.config({ virtual_lines = cur and false or { current_line = true } })
 end, { desc = "Toggle diagnostics message" })
+
+vim.keymap.set("n", "<leader>cy", function()
+  local diags = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
+  if #diags == 0 then
+    vim.notify("No diagnostics on line", vim.log.levels.INFO)
+    return
+  end
+  local lines = vim.tbl_map(function(d)
+    local code = d.code and tostring(d.code) .. ": " or ""
+    local src = d.source and " (" .. d.source .. ")" or ""
+    return code .. d.message .. src
+  end, diags)
+  vim.fn.setreg("+", table.concat(lines, "\n"))
+  vim.notify("Yanked " .. #diags .. " diagnostic(s)", vim.log.levels.INFO)
+end, { desc = "Yank line diagnostics" })
